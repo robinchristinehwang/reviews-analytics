acf715503f12cc7030ba72f0f3c4fa1daa85a2b5
Robin Hwang <s60439rd@gmail.com>
2021-12-19 20:17:36 +0800

    average length

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -7,3 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 100000 == 0:
 			print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+comment_len = 0
+for d in data:
+	comment_len = comment_len + len(d)
+
+print('average length is', comment_len/len(data))
+
